397a6bff908259270247bfdfb9887ccd427aadfb
amrit gautam <agautam@localhost.localdomain>
2021-03-18 13:54:40 -0400

    updated the executable to run according to project 2 modifications.

--- runproject.sh
+++ runproject.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
-python3 $PWD/python/Simulator.py -Nexp ${1:-10000} -Nmes ${2:-10} -temp ${3:-20} -seed ${4:-430} -output ${5:-"H0.txt"} -cause ${6:-"HF"}
-python3 $PWD/python/Simulator.py -Nexp ${1:-10000} -Nmes ${2:-10} -temp ${3:-20} -seed ${4:-450} -output ${7:-"H1.txt"} -cause ${8:-"ED"}
+python3 $PWD/python/Simulator.py -Nexp ${1:-1000} -Nmes ${2:-10}  -seed ${3:-430} -output ${4:-"H0.txt"} -cause ${5:-"HF"}
+python3 $PWD/python/Simulator.py -Nexp ${1:-1000} -Nmes ${2:-10}  -seed ${3:-450} -output ${6:-"H1.txt"} -cause ${7:-"ED"}
 #echo $7
 #echo $5
 python3 $PWD/python/ElectricityUse.py -input0 ${5:-H0.txt} -input1 ${7:-H1.txt}
