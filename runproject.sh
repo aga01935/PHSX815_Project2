29fb9f2a3f8234c0fb95fe7f27cc9ef99e89fce1
amrit gautam <agautam@localhost.localdomain>
2021-03-24 15:53:08 -0400

    moved the executables to exec folder

--- runproject.sh
+++ runproject.sh
@@ -1,6 +1,1 @@
-#! /usr/bin/env python3
-python3 $PWD/python/Simulator.py -Nexp ${1:-1000} -Nmes ${2:-10}  -seed ${3:-430} -output ${4:-"H0.txt"} -cause ${5:-"HF"}
-python3 $PWD/python/Simulator.py -Nexp ${1:-1000} -Nmes ${2:-10}  -seed ${3:-450} -output ${6:-"H1.txt"} -cause ${7:-"ED"}
-#echo $7
-#echo $5
-python3 $PWD/python/ElectricityUse.py -input0 ${5:-H0.txt} -input1 ${7:-H1.txt}
+exec/runproject.sh
